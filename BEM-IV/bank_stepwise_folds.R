44c3abfb2b33a632e147d25c17068774222f9f86
david-rope <70306258+david-rope@users.noreply.github.com>
2023-01-17 18:14:34 -0700

    Update bank_stepwise_folds.R

--- BEM-IV/bank_stepwise_folds.R
+++ BEM-IV/bank_stepwise_folds.R
@@ -23,7 +23,7 @@ library(MASS)
 kfolds <- 5
 totalIV <- matrix()
 for (i in 1:kfolds) { 
-  file_name <- paste("/home/david/R/PaperR/TestData/KFoldsCVSMOTE/Bank/DataSets/train_smote_bank_fold_", i, sep = "")
+  file_name <- paste(".../Bank/DataSets/train_smote_bank_fold_", i, sep = "")
   file_name <- paste(file_name, ".csv", sep = "")                                
   print(file_name)
   data_train <- read.csv(file_name, na = c("", "NA","?"))
@@ -63,7 +63,7 @@ for (i in 1:kfolds) {
   aucValueTrain<-AUC(predic_train, data_train$y)
   print(aucValueTrain)
     
-  model_name <- paste("/home/david/R/PaperR/TestData/KFoldsCVSMOTE/Bank/BEM/stepwise_model_bank_fold_", i, sep = "")
+  model_name <- paste(".../Bank/BEM/stepwise_model_bank_fold_", i, sep = "")
   model_name <- paste(model_name, ".rds", sep = "")                                
   print(model_name)
   saveRDS(backward.model, model_name)
