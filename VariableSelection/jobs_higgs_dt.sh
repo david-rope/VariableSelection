ba3046d19668c2eb49ee46487409fc71b1884651
david-rope <70306258+david-rope@users.noreply.github.com>
2023-01-18 18:45:09 -0700

    Update jobs_higgs_dt.sh

--- VariableSelection/jobs_higgs_dt.sh
+++ VariableSelection/jobs_higgs_dt.sh
@@ -2,7 +2,8 @@
 
 BATCH='qsub'
 
-INPUTDIR=/data1/David/BashR/KFoldsCV/Higgs/ScriptsDT_F5
+# Check script/ directory
+INPUTDIR=/Higgs/ScriptsDT_F5
 
 # Where am I?
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
