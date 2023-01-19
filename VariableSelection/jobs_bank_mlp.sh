b985bc73666dd37a9b8d651b6f4d1767d9431294
david-rope <70306258+david-rope@users.noreply.github.com>
2023-01-18 18:39:35 -0700

    Update jobs_bank_mlp.sh

--- VariableSelection/jobs_bank_mlp.sh
+++ VariableSelection/jobs_bank_mlp.sh
@@ -2,7 +2,8 @@
 
 BATCH='qsub'
 
-INPUTDIR=/data1/David/BashR/KFoldsCV/Bank/ScriptsMLP_F5
+# Check script/ directory
+INPUTDIR=/Bank/ScriptsMLP_F5
 
 # Where am I?
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
